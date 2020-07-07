3d22bc1b98160c4602c774b1ba7196526f672a46
Brent Cameron <bjcameron@gmail.com>
2020-07-07 13:47:34 -0600

    properly initalizes the board with square colours, and can print it nicely

--- lib/board.rb
+++ lib/board.rb
@@ -1,45 +1,37 @@
-require_relative 'piece.rb'
 require_relative 'pawn.rb'
-
+require_relative 'player.rb'
+require_relative 'cell.rb'
 
 BOARD_X = 0..7
 BOARD_Y = 0..7
 
+BG_BLACK = "\e[40m   \e[0m"
+BG_WHITE = "\e[47m   \e[0m"
+# HIGHTLIGHT = {
+#   INITIAL: "\e[44m   \e[0m",
+#   BLANK: "\e[42m   \e[0m",
+#   ENEMY: "\e[41m   \e[0m"
+# }
+
 class Board 
-  #this class will hold positions of every piece.
-  # this can be used to serialize, save game and later load the same
-  def initalize()
-    @board = Array.new(8){Array.new(8)}
-    @pieces = beginning_pieces()
+  attr_accessor :board
+  def initialize()
+    @board = build_board()
   end
 
-  def to_s
-    #want to factor this so it nicely prints the board with all the pieces where they should be
-  end
 
-  def beginning_pieces
-    player_1 = Player.new("brent", "black")
-    player_2 = Player.new("tnerb", "white")
+  def build_board()
+    # puts "hi im buildin"
+    Array.new(8) do |index_x|
+      Array.new(8) do |index_y|
+        if index_x % 2 == 0
+          index_y % 2 == 0 ?  Cell.new(BG_WHITE) : Cell.new(BG_BLACK)
+        else
+          index_y % 2 == 0 ? Cell.new(BG_BLACK) : Cell.new(BG_WHITE)
+        end
+      end
 
-    pieces = []
-    x = 0
-    8.times do 
-      #pawns
-      new_piece = { "piece" => Pawn.new(player_1, position = [x, 6] )}
-      white_piece = { "piece" => Pawn.new(player_2, position = [x, 1])}
-      @board[x][6] = new_piece
-      @board[x][1] = white_piece
-
-      pieces << new_piece
-      pieces << white_piece
-      x += 1
-      
     end
-
-    puts "pieces #{pieces}"
-    return pieces
-    #method to initalize all the pieces on the board with their positions.
-    #perhaps an array of hashes to store the piece object along with their place on the board
   end
 
   def on_game_board?(position)
@@ -52,11 +44,59 @@ class Board
   end
 
   def cell_contents(x, y)
-    @board[x][y] unless @board.nil?
+    @board[x][y].piece.nil? ? "empty square" : @board[x][y].piece
+    # @board[x][y].piece unless @board[x][y].piece.nil?
   end
+
+  def rando 
+    player = Player.new("Brent", "white")
+    @board[0][7].piece = Pawn.new(player, [0,7])
+      # @board[0][7].piece = "HELLO"
+
+  end
+
+  def display_board
+    puts "\n"
+    puts "   -----------------------------------------------"
+    puts "8 |  #{@board[0][7]}  |  #{@board[1][7]}  |  #{@board[2][7]}  |  #{@board[3][7]}  |  #{@board[4][7]}  |  #{@board[5][7]}  |  #{@board[6][7]}  |  #{@board[7][7]}  |"
+    puts seperator = "   -------+-------+-------+-------+-------+-------+-------+-------"
+    puts "7 |  #{@board[0][6]}  |  #{@board[1][6]}  |  #{@board[2][6]}  |  #{@board[3][6]}  |  #{@board[4][6]}  |  #{@board[5][6]}  |  #{@board[6][6]}  |  #{@board[7][6]}  |"
+    puts seperator
+    puts "6 |  #{@board[0][5]}  |  #{@board[1][5]}  |  #{@board[2][5]}  |  #{@board[3][5]}  |  #{@board[4][5]}  |  #{@board[5][5]}  |  #{@board[6][5]}  |  #{@board[7][5]}  |"
+    puts seperator
+    puts "5 |  #{@board[0][4]}  |  #{@board[1][4]}  |  #{@board[2][4]}  |  #{@board[3][4]}  |  #{@board[4][4]}  |  #{@board[5][4]}  |  #{@board[6][4]}  |  #{@board[7][4]}  |"
+    puts seperator
+    puts "4 |  #{@board[0][3]}  |  #{@board[1][3]}  |  #{@board[2][3]}  |  #{@board[3][3]}  |  #{@board[4][3]}  |  #{@board[5][3]}  |  #{@board[6][3]}  |  #{@board[7][3]}  |"
+    puts seperator
+    puts "3 |  #{@board[0][2]}  |  #{@board[1][2]}  |  #{@board[2][2]}  |  #{@board[3][2]}  |  #{@board[4][2]}  |  #{@board[5][2]}  |  #{@board[6][2]}  |  #{@board[7][2]}  |"
+    puts seperator
+    puts "2 |  #{@board[0][1]}  |  #{@board[1][1]}  |  #{@board[2][1]}  |  #{@board[3][1]}  |  #{@board[4][1]}  |  #{@board[5][1]}  |  #{@board[6][1]}  |  #{@board[7][1]}  |"
+    puts seperator
+    puts "1 |  #{@board[0][0]}  |  #{@board[1][0]}  |  #{@board[2][0]}  |  #{@board[3][0]}  |  #{@board[4][0]}  |  #{@board[5][0]}  |  #{@board[6][0]}  |  #{@board[7][0]}  |"
+    puts "   -----------------------------------------------"
+    puts "       A       B       C       D       E       F       G       H"
+    puts "\n"
+  end
+
 end
 
 
 test = Board.new
-puts "cell"
-puts test.cell_contents(0,6)
+# puts test.cell_contents(1,0)
+
+# puts test
+test.display_board
+test.rando
+
+test.display_board
+
+
+# x = 0 
+# 8.times do 
+#   puts x
+#   puts test.board[x]
+#   x+=1
+# end
+
+# puts BG_WHITE
+# puts BG_BLACK
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,26 +1,3 @@
-# require './lib/calculator'
-
-# describe Calculator do 
-#   describe "#add" do
-#     it "returns the sum of two numbers" do
-#       calculator = Calculator.new
-#       expect(calculator.add(5,2)).to eql(7)
-#     end
-
-#     it "returns the sum of more than two numbers" do
-#       calculator = Calculator.new
-#       expect(calculator.add(2,5,7)).to eql(14)
-#     end
-#   end
-#   describe "#multiply" do
-#     it "returns the product of two numbers" do 
-#       calculator = Calculator.new
-#       expect(calculator.multiply(5,2)).to eql(10)
-#     end
-#   end
-# end
-
-
 require './lib/pawn.rb'
 require './lib/board.rb'
 require './lib/piece.rb'
@@ -31,6 +8,14 @@ require './lib/rook.rb'
 
 describe Board do
   describe "#initialize" do 
+    it "actually initializes the board, aka board is not nil" do 
+      test_board = Board.new
+      expect(test_board.nil?).to eql(false)
+    end
+    it "initializes cells with their appropriate background colour " do
+      test_board = Board.new
+      expect(@board[0][0]).to eql("\e[47m   \e[0m")
+    end
     it "fills a 2-d array with pieces where they belong" do
       board = Board.new
       expect(board.cell_contents(0,6)).to eql("pawn")
@@ -39,6 +24,25 @@ describe Board do
       # expect(pieces)
     end
        
+
+  end
+end
+
+describe Pawn do
+  describe "#initialize" do
+    it "sets its identity to pawn" do 
+      player = Player.new("Brent", "black")
+      pawn = Pawn.new(player, [0,1])
+      #expect off the beginning there are 2 eligible moves
+      #expect(pawn.eligible_moves).length.to eql(2)
+      expect(pawn.piece).to eql("pawn")
+    end
+    it "has the correct 2 moves to start off " do
+      player = Player.new("Brent", "black")
+      pawn = Pawn.new(player, [0,1])
+      #expect off the beginning there are 2 eligible moves
+      expect(pawn.eligible_moves).length.to eql(2)
+    end
 
   end
 end
